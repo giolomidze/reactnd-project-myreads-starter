83e1c3a54ca4d31deed65bb57e92bc0dd50451aa
Giorgi Lomidze <gio@developer.ge>
2018-09-30 16:45:17 -0400

    feat: render shelves dynamically
    Render shelves dynamically in case we would need to change an order for example.

--- src/ListBooks.js
+++ src/ListBooks.js
@@ -3,57 +3,61 @@ import './App.css';
 
 export class ListBooks extends Component {
     render() {
-        const { books } = this.props;
-        console.log(books);
+        const { books, shelf } = this.props;
         return (
             <div className="bookshelf">
-                <h2 className="bookshelf-title">Currently Reading</h2>
+                <h2 className="bookshelf-title">{shelf.title}</h2>
                 <div className="bookshelf-books">
                     <ol className="books-grid">
-                        {books.map(book => (
-                            <li key={book.id}>
-                                <div className="book">
-                                    <div className="book-top">
-                                        <div
-                                            className="book-cover"
-                                            style={{
-                                                width: 128,
-                                                height: 193,
-                                                backgroundImage: `url(${
-                                                    book.imageLinks.thumbnail
-                                                })`,
-                                            }}
-                                        />
-                                        <div className="book-shelf-changer">
-                                            <select>
-                                                <option value="move" disabled>
-                                                    Move to...
-                                                </option>
-                                                <option value="currentlyReading">
-                                                    Currently Reading
-                                                </option>
-                                                <option value="wantToRead">
-                                                    Want to Read
-                                                </option>
-                                                <option value="read">
-                                                    Read
-                                                </option>
-                                                <option value="none">
-                                                    None
-                                                </option>
-                                            </select>
+                        {books
+                            .filter(book => book.shelf === shelf.type)
+                            .map(book => (
+                                <li key={book.id}>
+                                    <div className="book">
+                                        <div className="book-top">
+                                            <div
+                                                className="book-cover"
+                                                style={{
+                                                    width: 128,
+                                                    height: 193,
+                                                    backgroundImage: `url(${
+                                                        book.imageLinks
+                                                            .thumbnail
+                                                    })`,
+                                                }}
+                                            />
+                                            <div className="book-shelf-changer">
+                                                <select>
+                                                    <option
+                                                        value="move"
+                                                        disabled
+                                                    >
+                                                        Move to...
+                                                    </option>
+                                                    <option value="currentlyReading">
+                                                        Currently Reading
+                                                    </option>
+                                                    <option value="wantToRead">
+                                                        Want to Read
+                                                    </option>
+                                                    <option value="read">
+                                                        Read
+                                                    </option>
+                                                    <option value="none">
+                                                        None
+                                                    </option>
+                                                </select>
+                                            </div>
+                                        </div>
+                                        <div className="book-title">
+                                            {book.title}
+                                        </div>
+                                        <div className="book-authors">
+                                            {book.authors.map(author => author)}
                                         </div>
                                     </div>
-                                    <div className="book-title">
-                                        {book.title}
-                                    </div>
-                                    <div className="book-authors">
-                                        {book.authors.map(author => author)}
-                                    </div>
-                                </div>
-                            </li>
-                        ))}
-
+                                </li>
+                            ))}
                         <li />
                     </ol>
                 </div>
