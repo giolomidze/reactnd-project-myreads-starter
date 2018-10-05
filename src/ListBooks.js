86b869123db39d3b2d132d9f5dd740e5841158b1
Giorgi Lomidze <gio@developer.ge>
2018-10-04 21:25:44 -0400

    refactor: convert ListBooks to stateless component
    ListBooks doesn't have its own state and its only porpuse is to render the books thus it's being converted to stateless component.

--- src/ListBooks.js
+++ src/ListBooks.js
@@ -1,54 +1,52 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
 import PropTypes from 'prop-types';
 
-class ListBooks extends Component {
-  render() {
-    const { books, changeShelf } = this.props;
-    return (
-      <ol className="books-grid">
-        {books.map(book => (
-          <li key={book.id}>
-            <div className="book">
-              <div className="book-top">
-                <div
-                  className="book-cover"
-                  style={{
-                    width: 128,
-                    height: 193,
-                    backgroundImage: `url(${
-                      book.imageLinks && book.imageLinks.thumbnail
-                        ? book.imageLinks.thumbnail
-                        : ''
-                    })`,
-                  }}
-                />
-                <div className="book-shelf-changer">
-                  <select
-                    value={book.shelf}
-                    onChange={event => changeShelf(book, event.target.value)}
-                  >
-                    <option value="move" disabled>
-                      Move to...
-                    </option>
-                    <option value="currentlyReading">Currently Reading</option>
-                    <option value="wantToRead">Want to Read</option>
-                    <option value="read">Read</option>
-                    <option value="none">None</option>
-                  </select>
-                </div>
-              </div>
-              <div className="book-title">{book.title}</div>
-              <div className="book-authors">
-                {book.authors ? book.authors.map(author => author) : ''}
+const ListBooks = props => {
+  const { books, changeShelf } = props;
+  return (
+    <ol className="books-grid">
+      {books.map(book => (
+        <li key={book.id}>
+          <div className="book">
+            <div className="book-top">
+              <div
+                className="book-cover"
+                style={{
+                  width: 128,
+                  height: 193,
+                  backgroundImage: `url(${
+                    book.imageLinks && book.imageLinks.thumbnail
+                      ? book.imageLinks.thumbnail
+                      : ''
+                  })`,
+                }}
+              />
+              <div className="book-shelf-changer">
+                <select
+                  value={book.shelf}
+                  onChange={event => changeShelf(book, event.target.value)}
+                >
+                  <option value="move" disabled>
+                    Move to...
+                  </option>
+                  <option value="currentlyReading">Currently Reading</option>
+                  <option value="wantToRead">Want to Read</option>
+                  <option value="read">Read</option>
+                  <option value="none">None</option>
+                </select>
               </div>
             </div>
-          </li>
-        ))}
-      </ol>
-    );
-  }
-}
+            <div className="book-title">{book.title}</div>
+            <div className="book-authors">
+              {book.authors ? book.authors.map(author => author) : ''}
+            </div>
+          </div>
+        </li>
+      ))}
+    </ol>
+  );
+};
 
 ListBooks.propTypes = {
   books: PropTypes.array.isRequired,
